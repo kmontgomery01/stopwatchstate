7b9d7a4fb3f5d5c80bb0d434da61d30676eb0494
KMontgomery01@GMail.com <KMontgomery01@Gmail.com>
2019-09-30 07:52:43 -0400

    firstStopwatchCommit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import StopWatch from './StopWatch';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const model={
+    running: false,
+    time: 0
+  }
+
+ReactDOM.render(<StopWatch model={model}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
